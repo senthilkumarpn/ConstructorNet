c205c1e9f391cc54d2cd235ba0625c3348e0da34
madhusudhanvenkatesan-github <65552529+madhusudhanvenkatesan-github@users.noreply.github.com>
2020-08-18 19:17:27 +0530

    Base Solution uploaded

---
+++ Cstor/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Cstor
+{
+    public class Car
+    {
+        public int wheels;
+        public Car()
+        {
+            wheels = 4;
+        }
+
+        public Car(int tires)
+        {
+            wheels = tires;
+
+        }
+    }
+}
---
+++ Cstor/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Cstor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car mycar = new Car();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
